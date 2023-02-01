100df38093c2cd192a8ffaab0340a697af8ecb92
Melvin <melvinhkusuma@gmail.com>
2023-02-01 17:51:56 +1100

    made replies into a component

--- src/utils.js
+++ src/utils.js
@@ -35,23 +35,23 @@ export function timeSince(date) {
     var interval = seconds / 31536000;
 
     if (interval > 1) 
-        return Math.floor(interval) + " years ago";
+        return Math.floor(interval) + ` year${Math.floor(interval) === 1 ? '' : 's'} ago`;
     
     interval = seconds / 2592000;
     if (interval > 1) 
-        return Math.floor(interval) + " months ago";
+        return Math.floor(interval) + ` month${Math.floor(interval) === 1 ? '' : 's'} ago`;
     
     interval = seconds / 86400;
     if (interval > 1) 
-        return Math.floor(interval) + " days ago";
+        return Math.floor(interval) + ` day${Math.floor(interval) === 1 ? '' : 's'} ago`;
     
     interval = seconds / 3600;
     if (interval > 1) 
-        return Math.floor(interval) + " hours ago";
+        return Math.floor(interval) + ` hour${Math.floor(interval) === 1 ? '' : 's'} ago`;
     
     interval = seconds / 60;
     if (interval > 1) 
-        return Math.floor(interval) + " minutes ago";
+        return Math.floor(interval) + ` minute${Math.floor(interval) === 1 ? '' : 's'} ago`;
     
-    return Math.floor(seconds) + " seconds ago";
+    return Math.floor(seconds) + ` second${Math.floor(interval) === 1 ? '' : 's'} ago`;
 }
