cbe47c129211cb4a1f92156c19a52cfe741753d5
Melvin <melvinhkusuma@gmail.com>
2023-02-02 16:21:51 +1100

    started hierarchical replies

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -3,6 +3,7 @@ import parse from 'html-react-parser'
 import Replies from "./Replies"
 import { useState } from "react"
 import ReplyIcon from '@mui/icons-material/Reply';
+import '../App.css'
 
 function Comment({ comment }) {
     const replyLength = comment.childrenIDs.length
@@ -12,10 +13,15 @@ function Comment({ comment }) {
     return (
         <>
             <li onMouseEnter={() => setShowTooltip(true)} onMouseLeave={() => setShowTooltip(false)}>
-                <span className='comment-date' title={comment.date}>
-                    {timeSince(comment.createdAt.toDate())}
+                <span className='comment-header'>
+                    <span title={comment.date}>
+                        {timeSince(comment.createdAt.toDate())}
+                    </span>
+                    <span style={{ visibility: showTooltip ? 'visible' : 'hidden', cursor: 'pointer' }}>
+                        <ReplyIcon className='reply-btn' fontSize='small'/>
+                        Reply
+                    </span>
                 </span>
-                <ReplyIcon style={{ visibility: showTooltip ? 'visible' : 'hidden' }} />
                 {parse(comment.description)}
             </li>
             {replyLength > 0 ?
